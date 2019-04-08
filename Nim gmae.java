cf3f19835d08a58e062c943f3a6ce4ee7ebe13bb
Saumen <saumenroy323@gmail.com>
2019-04-08 12:05:53 +0600

    Add files via upload

---
+++ Nim gmae.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n<0){
+            return false;
+        }
+        if(n%4 == 0){
+            return false;
+        }
+        return true;
+        
+    }
+}
